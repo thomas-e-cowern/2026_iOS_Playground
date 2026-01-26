17c181788a3dc4f1afa8d9c6c841f5b44df74eae
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-26 14:35:26 -0500

    update StateModifier with view extension

--- Rebeloper/LoadingStates/LoadingStates/Overlays/StateModifier.swift
+++ Rebeloper/LoadingStates/LoadingStates/Overlays/StateModifier.swift
@@ -36,3 +36,26 @@ struct StateModifier<OverlayContent: View>: ViewModifier {
             .animation(.easeInOut(duration: 0.5), value: state)
     }
 }
+
+extension View {
+    func asyncOverlay<OverlayContent: View>(
+        state: AsyncUIState,
+        isBlocking: Bool = true,
+        onRetry: (@MainActor () async -> Void)? = nil,
+        @ViewBuilder overlay: @escaping (_ state: AsyncUIState, _ retry: (@MainActor () async -> Void)?) -> OverlayContent
+    ) -> some View {
+        modifier(StateModifier(state: state, isBlocking: isBlocking, onRetry: onRetry, overlay: overlay))
+    }
+}
+
+private struct DefaultAsyncOverlay: View {
+    
+    let state: AsyncUIState
+    let retry: (@MainActor () async -> Void)?
+    
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
