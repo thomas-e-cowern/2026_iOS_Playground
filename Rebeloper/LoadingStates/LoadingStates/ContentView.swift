d95e62513e5416e3987dee867e7b1cd5c43361a8
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-26 13:23:55 -0500

    LoadingStates initial commit in Rebeloper folder

---
+++ Rebeloper/LoadingStates/LoadingStates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoadingStates
+//
+//  Created by Thomas Cowern on 1/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rebeloper/LoadingStates/LoadingStates/LoadingStatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingStatesApp.swift
+//  LoadingStates
+//
+//  Created by Thomas Cowern on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct LoadingStatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
