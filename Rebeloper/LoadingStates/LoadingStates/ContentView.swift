0a2c79932e812642aec909639ebf4ebbe57b2dbd
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-26 15:08:26 -0500

    update ContentView, complete functions and Button actions to use state modifier

--- Rebeloper/LoadingStates/LoadingStates/ContentView.swift
+++ Rebeloper/LoadingStates/LoadingStates/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var state: AsyncUIState = .idle
+    
     var body: some View {
         VStack {
             List {
@@ -29,25 +32,39 @@ struct ContentView: View {
                     }
                 }
             }
+            .navigationTitle("Loading States")
+            .asyncOverlay(state: state) {
+                await loadSuccess()
+            }
         }
     }
     
     @MainActor
     private func loadSuccess() async {
-            
+        state = .loading
+        try? await Task.sleep(nanoseconds: 1_000_000_000)
+        state = .idle
     }
     
     @MainActor
     private func loadEmpty() async {
-        
+        state = .loading
+        try? await Task.sleep(nanoseconds: 1_000_000_000)
+        state = .empty(message: "There are no items to load...")
+        try? await Task.sleep(nanoseconds: 3_000_000_000)
+        state = .idle
     }
     
     @MainActor
     private func loadFail() async {
-        
+        state = .loading
+        try? await Task.sleep(nanoseconds: 1_000_000_000)
+        state = .failure(message: "There was no way to get the data...")
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
