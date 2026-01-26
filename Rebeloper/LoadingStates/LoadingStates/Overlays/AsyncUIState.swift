d1f09f368fc921d65d8af69cabedb1a16007d8f3
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-26 13:40:52 -0500

    update xcuserstate

--- Rebeloper/LoadingStates/LoadingStates/Overlays/AsyncUIState.swift
+++ Rebeloper/LoadingStates/LoadingStates/Overlays/AsyncUIState.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+enum AsyncUIState: Equatable {
+    case idle
+    case loading
+    case empty(message: String = "This is a default error....")
+    case failure(message: String) // error message come from localized description
+}
