0c792bcb20267f34ba593c22233b0c7111e31552
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-28 11:54:07 -0500

    update CapsuleView to fix Preview

--- Other/ErrorHandling/ErrorHandling/CapsuleView.swift
+++ Other/ErrorHandling/ErrorHandling/CapsuleView.swift
@@ -20,5 +20,5 @@ struct CapsuleView: View {
 
 #Preview {
 //    CapsuleView(capsule: Capsule(reuseCount: 0, waterLandings: 1, landLandings: 0, lastUpdate: "Hanging in atrium at SpaceX HQ in Hawthorne", launches: ["5eb87cdeffd86e000604b330"], serial: "C101", status: "retired", type: "Dragon 1.0", id: "5e9e2c5bf35918ed873b2664"))
-    CapsuleView(capsule: Capsule(id: "123455667"))
+    CapsuleView(capsule: Capsule(id: "1234567890"))
 }
