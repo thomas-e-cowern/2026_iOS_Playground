43f2a806fdeaac244480cc475b15083b37953555
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-28 11:53:00 -0500

    add ComboController to Models folder

--- Other/ErrorHandling/ErrorHandling/Models/ComboController.swift
+++ Other/ErrorHandling/ErrorHandling/Models/ComboController.swift
@@ -4,5 +4,21 @@
 //
 //  Created by Thomas Cowern on 1/28/26.
 //
+import SwiftUI
+import Observation
 
-import Foundation
+@Observable
+class ComboController {
+
+    var capsules: [Capsule] = []
+    let apiService: APIService
+    
+    init(apiService: APIService) {
+        self.apiService = apiService
+    }
+    
+    func getAllCapsules() async throws {
+        self.capsules = try await apiService.getAllCapsules()
+    }
+}
+
