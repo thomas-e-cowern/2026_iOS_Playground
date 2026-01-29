7da7665ccae7fa4e31952a304f35e7d008917f13
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-29 07:41:01 -0500

    CustomTavView initial commit in Rebeloper folder

---
+++ Rebeloper/CustomTabView/CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by Thomas Cowern on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
