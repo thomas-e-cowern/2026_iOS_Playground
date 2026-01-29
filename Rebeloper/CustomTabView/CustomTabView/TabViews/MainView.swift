ed3c65339a664334df5494e34fa2563a9474bd61
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-29 07:55:19 -0500

    add MainView to TabViews folder

---
+++ Rebeloper/CustomTabView/CustomTabView/TabViews/MainView.swift
@@ -0,0 +1,34 @@
+//
+//  MainView.swift
+//  CustomTabView
+//
+//  Created by Thomas Cowern on 1/29/26.
+//
+
+import SwiftUI
+
+fileprivate struct CustomTabItemView: View {
+    
+    let systemName: String
+    let text: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Image(systemName: systemName)
+                .font(.title)
+            Text(text)
+                .font(.caption)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
