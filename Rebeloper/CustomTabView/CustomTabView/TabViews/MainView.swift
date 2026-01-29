65b90aedfd38d28becb8a1a32c2d0218d9f863a8
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-29 08:00:46 -0500

    update MainView, add CustomTabItemView

--- Rebeloper/CustomTabView/CustomTabView/TabViews/MainView.swift
+++ Rebeloper/CustomTabView/CustomTabView/TabViews/MainView.swift
@@ -23,6 +23,8 @@ fileprivate struct CustomTabItemView: View {
     }
 }
 
+
+
 struct MainView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
