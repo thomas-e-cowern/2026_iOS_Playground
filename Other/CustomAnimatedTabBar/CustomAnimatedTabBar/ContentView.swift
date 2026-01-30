81fa98a3eb3948c83786e0c79fee5acd507edc31
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-30 09:05:36 -0500

    update ContentView, add switch statement to use new Views

--- Other/CustomAnimatedTabBar/CustomAnimatedTabBar/ContentView.swift
+++ Other/CustomAnimatedTabBar/CustomAnimatedTabBar/ContentView.swift
@@ -13,15 +13,35 @@ struct ContentView: View {
     @State private var selectedTab: Tab = .house
     
     init() {
-        UITabBar.appearance().isHidden = false
+        UITabBar.appearance().isHidden = true
     }
     
     var body: some View {
-        VStack {
-            Spacer()
-            CustomTabBar(selectedTab: $selectedTab)
+        ZStack {
+            VStack {
+                TabView(selection: $selectedTab) {
+                    switch selectedTab {
+                    case .house:
+                        House()
+                    case .message:
+                        Message()
+                    case .person:
+                        Person()
+                    case .leaf:
+                        Leaf()
+                    case .gearshape:
+                        Gear()
+                    }
+                }
+            }
+            
+            
+            VStack {
+                Spacer()
+                CustomTabBar(selectedTab: $selectedTab)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
