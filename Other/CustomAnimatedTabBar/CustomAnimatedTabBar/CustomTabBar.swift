06ced0f02d6ee77429d1b70546efcc2e9213da82
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-30 08:40:59 -0500

    add CustomTabBar to project

--- Other/CustomAnimatedTabBar/CustomAnimatedTabBar/CustomTabBar.swift
+++ Other/CustomAnimatedTabBar/CustomAnimatedTabBar/CustomTabBar.swift
@@ -7,12 +7,46 @@
 
 import SwiftUI
 
+enum Tab: String, CaseIterable, Hashable {
+    case house
+    case message
+    case person
+    case leaf
+    case gearshape
+}
+
 struct CustomTabBar: View {
+    
+    @Binding var selectedTab: Tab
+    private var fillImage: String {
+        selectedTab.rawValue + ".fill"
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                ForEach(Tab.allCases, id: \.self) { tab in
+                    Spacer()
+                    Image(systemName: selectedTab == tab ? fillImage : tab.rawValue)
+                        .scaleEffect(tab == selectedTab ? 1.25 : 1.0)
+                        .foregroundStyle(selectedTab == tab ? .red : .secondary)
+                        .font(.system(size: 22))
+                        .onTapGesture {
+                            withAnimation(.easeIn(duration: 0.5)) {
+                                selectedTab = tab
+                            }
+                        }
+                    Spacer()
+                }
+            }
+            .frame(width: nil, height: 60)
+            .background(.thinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding()
+        }
     }
 }
 
 #Preview {
-    CustomTabBar()
+    CustomTabBar(selectedTab: .constant(.house))
 }
