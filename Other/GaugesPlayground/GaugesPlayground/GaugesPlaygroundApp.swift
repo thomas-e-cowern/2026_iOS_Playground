6119f58035f90c15739b6757b21ce91a9241ccc4
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-03 14:06:56 -0500

    GaugesPlayground initial commit in Other folder

---
+++ Other/GaugesPlayground/GaugesPlayground/GaugesPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugesPlaygroundApp.swift
+//  GaugesPlayground
+//
+//  Created by Thomas Cowern on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GaugesPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
