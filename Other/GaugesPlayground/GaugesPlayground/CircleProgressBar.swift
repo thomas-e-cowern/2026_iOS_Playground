837cdc0d11b8992bf47f0ead391aed4d7762cd7b
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-04 16:17:43 -0500

    add CircleProgressBar to project

--- Other/GaugesPlayground/GaugesPlayground/CircleProgressBar.swift
+++ Other/GaugesPlayground/GaugesPlayground/CircleProgressBar.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct CircleProgressBar: View {
+    let progress: Double // Value from 0 to 100
+    let size: Double
+    var foregroundColor: Color = .blue
+    var backgroundColor: Color = .gray.opacity(0.3)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack(alignment: .center) {
+            // Background shape
+            Circle()
+                .fill(backgroundColor)
+                .frame(height: size)
+            
+            // Foreground shape (the actual progress)
+            Circle()
+                .fill(foregroundColor)
+                .frame(height: size * (progress / 100))
+            
+            Text("Complete!")
+                .font(.title2)
+                .foregroundStyle(Color.white)
+                .opacity(progress == 100 ? 1 : 0)
+        }
     }
 }
 
 #Preview {
-    CircleProgressBar()
+    CircleProgressBar(progress: 90, size: 150)
 }
