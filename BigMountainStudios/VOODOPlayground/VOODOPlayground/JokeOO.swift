10464507b86ac5e0bb9b4727bc24a0332d81da22
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-11 09:32:56 -0500

    add Joke00 file to project

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/JokeOO.swift
@@ -0,0 +1,31 @@
+//
+//  JokeOO.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/11/26.
+//
+
+import Foundation
+import Observation
+
+@Observable
+class JokeOO {
+    var joke: JokeModel?
+    
+    func fetchJoke() async {
+        guard let url = URL(string: "https://v2.jokeapi.dev/joke/Any") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            let decodedResponse = try JSONDecoder().decode(JokeModel.self, from: data)
+            
+            joke = decodedResponse
+        } catch {
+            print("There were errors decoding the joke: \(error.localizedDescription)")
+        }
+    }
+}
