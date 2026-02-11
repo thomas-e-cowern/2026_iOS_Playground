643f73b64ac5d1cad0df5a0036f52378f13f6d77
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-11 10:34:33 -0500

    renamed JokeModel to Joke

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Joke.swift
@@ -0,0 +1,27 @@
+//
+//  JokeModel.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/11/26.
+//
+
+import Foundation
+
+// MARK: - Joke
+struct Joke: Codable, Identifiable {
+    let error: Bool
+    let category: String
+    let type: String
+//    let joke: String
+    let flags: Flags
+    let safe: Bool
+    let id: Int
+    let lang: String
+}
+
+// MARK: - Flags
+struct Flags: Codable {
+    let nsfw, religious, political, racist: Bool
+    let sexist, explicit: Bool
+}
+
