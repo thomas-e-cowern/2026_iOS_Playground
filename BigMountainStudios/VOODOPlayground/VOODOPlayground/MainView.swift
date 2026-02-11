9f51e27c6178df82cd10786836b5054b45fa176c
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-11 08:43:02 -0500

    add BigMountainStudios folder and VOODOPlayground

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/MainView.swift
@@ -0,0 +1,49 @@
+//
+//  MainView.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/11/26.
+//
+
+// View
+import SwiftUI
+
+struct MainViewView: View {
+    @State private var oo = MainViewOO()
+    
+    var body: some View {
+        List(oo.data) { datum in
+            Text(datum.name)
+        }
+        .task {
+            oo.fetch()
+        }
+    }
+}
+
+#Preview {
+    MainViewView()
+}
+
+// Observable Object
+import Observation
+import SwiftUI
+
+@Observable
+class MainViewOO {
+    var data: [MainViewDO] = []
+    
+    func fetch() {
+        data = [MainViewDO(name: "Datum 1"),
+                MainViewDO(name: "Datum 2"),
+                MainViewDO(name: "Datum 3")]
+    }
+}
+
+// Data Object
+import Foundation
+
+struct MainViewDO: Identifiable {
+    let id = UUID()
+    var name: String
+}
---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/VOODOPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  VOODOPlaygroundApp.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct VOODOPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
