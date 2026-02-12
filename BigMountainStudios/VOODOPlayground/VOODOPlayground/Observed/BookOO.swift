ee90333e2390500443ae10648fc1d70fb7502ae1
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-12 09:02:38 -0500

    add BookOO to Observed folder

--- BigMountainStudios/VOODOPlayground/VOODOPlayground/Observed/BookOO.swift
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Observed/BookOO.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class BookOO {
+    
+    var books: [Book] = []
+    
+    func fetchBooks() {
+        books = [
+            Book(title: "Casino Royale", author: "Ian Fleming"),
+            Book(title: "The Men Who Stare at Goats", author: "Jon Ronson"),
+            Book(title: "The Bible", author: "God"),
+            Book(title: "The Winds of War", author: "Herman Woulk")
+        ]
+    }
+}
