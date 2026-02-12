24175cb6093e478f884df39b53c00471d4f98658
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-12 09:01:29 -0500

    add Models folder, add Book model to Models folder

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Models/Book.swift
@@ -0,0 +1,13 @@
+//
+//  Book.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/12/26.
+//
+
+import Foundation
+
+struct Book: Codable {
+    let title: String
+    let author: String
+}
---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Observed/BookOO.swift
@@ -0,0 +1,8 @@
+//
+//  BookOO.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/12/26.
+//
+
+import Foundation
