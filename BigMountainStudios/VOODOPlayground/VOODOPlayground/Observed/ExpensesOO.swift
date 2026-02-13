a1c755b1dd1ec2fe16daff47a53318dc920b51da
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-13 10:24:50 -0500

    add ExpensesOO to Observed folder

--- BigMountainStudios/VOODOPlayground/VOODOPlayground/Observed/ExpensesOO.swift
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Observed/ExpensesOO.swift
@@ -5,4 +5,18 @@
 //  Created by Thomas Cowern on 2/13/26.
 //
 
-import Foundation
+import SwiftUI
+
+@Observable
+class ExpensesOO {
+    
+    var expenses: [Expense] = []
+    
+    func getExpenses() {
+        expenses = [
+            Expense(employeeName: "Barbara Smith", type: "Flight", amount: 580.00),
+            Expense(employeeName: "Barbara Smith", type: "Hotel", amount: 1600.00),
+            Expense(employeeName: "Barbara Smith", type: "Meals", amount: 418.00)
+        ]
+    }
+}
