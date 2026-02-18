2ef617540f084afa679c122038684c0453951957
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-18 09:45:14 -0500

    update BindingExamplesView, add isOn variable and Previews for both bool states

--- BigMountainStudios/VOODOPlayground/VOODOPlayground/Views/BindingExamplesView.swift
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Views/BindingExamplesView.swift
@@ -10,24 +10,40 @@ import SwiftUI
 struct BindingExamplesView: View {
     
     @State private var examples = BindingExamples()
+    @State var isOn: Bool
     
     var body: some View {
-        Form {
-            ColorPicker("Color Picker", selection: $examples.color)
-            DatePicker("Date", selection: $examples.date)
-            Slider(value: $examples.slider)
-            Stepper("Value \(examples.stepper)", value: $examples.stepper)
-            Text(examples.text)
-            TextField("Placeholder", text: $examples.textEditor)
-                .textFieldStyle(.roundedBorder)
-            TextEditor(text: $examples.textEditor)
-                .border(.blue)
-                .frame(height: 88)
-            Toggle("Toggle", isOn: $examples.toggle)
+        VStack {
+            Form {
+                ColorPicker("Color Picker", selection: $examples.color)
+                DatePicker("Date", selection: $examples.date)
+                Slider(value: $examples.slider)
+                Stepper("Value \(examples.stepper)", value: $examples.stepper)
+                Text(examples.text)
+                TextField("Placeholder", text: $examples.textEditor)
+                    .textFieldStyle(.roundedBorder)
+                TextEditor(text: $examples.textEditor)
+                    .border(.blue)
+                    .frame(height: 88)
+                Toggle("Toggle", isOn: $examples.toggle)
+                Text(isOn ? "Its On" : "Its Off")
+            }
+            
+            VStack {
+                Toggle("Is it on?", isOn: $isOn)
+            }
+            
+            
         }
+        .padding()
     }
 }
 
-#Preview {
-    BindingExamplesView()
+#Preview("Not On") {
+    BindingExamplesView(isOn: false)
 }
+
+#Preview("On") {
+    BindingExamplesView(isOn: true)
+}
+
