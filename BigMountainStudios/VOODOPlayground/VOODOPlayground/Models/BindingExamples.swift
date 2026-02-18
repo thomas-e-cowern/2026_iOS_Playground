831dd99a6ffc2f9e2bbaeee69f3a2009cc47b9f4
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-18 09:14:46 -0500

    add BindingExampels to Models folder

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Models/BindingExamples.swift
@@ -0,0 +1,19 @@
+//
+//  BindingExamples.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/18/26.
+//
+
+import SwiftUI
+
+struct BindingExamples {
+    var color = Color.blue
+    var date = Date()
+    var slider = 0.75
+    var stepper = 25
+    var text = "This is a one way binding"
+    var textField = "This is a two way binding"
+    var textEditor = "Text Editor stuff"
+    var toggle = true
+}
