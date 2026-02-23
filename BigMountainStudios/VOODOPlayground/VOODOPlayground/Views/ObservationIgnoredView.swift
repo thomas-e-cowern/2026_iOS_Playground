a6698f20722c087f38e52a7bc4553edd2e935ee0
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-23 08:31:56 -0500

    add ObservationIgnoredView to Views folder

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Views/ObservationIgnoredView.swift
@@ -0,0 +1,53 @@
+//
+//  ObservationIgnoredView.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/23/26.
+//
+
+import SwiftUI
+
+struct ObservationIgnoredView: View {
+    
+    @State private var list = BookList()
+    
+    var body: some View {
+        
+        List {
+            Section {
+                ForEach(list.list, id: \.self) { book in
+                    Text(book)
+                }
+            } header: {
+                Text("Book List")
+                    .font(.title.bold())
+                    .foregroundStyle(.black)
+            } footer: {
+                Text("\(list.footer)")
+                    .font(.headline)
+            }
+
+        }
+    }
+}
+
+#Preview {
+    ObservationIgnoredView()
+}
+
+@Observable
+class BookList {
+    var list = ["Book 1", "Book 2", "Book 3"]
+    var listName = "Book List"
+    
+    @ObservationIgnored var footer = "3 Books"
+    
+    func makeUpdates() {
+        list.append("Book \(list.count + 1)")
+        footer = "\(list.count) books"
+    }
+    
+    func justFoooterUpdate() {
+        footer = "Read all \(list.count) books!"
+    }
+}
