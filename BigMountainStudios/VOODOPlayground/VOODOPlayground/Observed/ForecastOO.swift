6e70a2b9f45cd1c59e65d98e85490467c55bab84
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-02-24 09:54:02 -0500

    add ForecastOO to Observable folder

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Observed/ForecastOO.swift
@@ -0,0 +1,41 @@
+//
+//  ForecastOO.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/24/26.
+//
+
+import Foundation
+
+@Observable
+class ForecastOO {
+    var sevenDays = [Weather]()
+    
+    struct Weather: Identifiable {
+        let id = UUID()
+        var day = ""
+        var icon = ""
+    }
+    
+    init() {
+        sevenDays = [
+            Weather(day: "Sunday", icon: "cloud.snow.fill"),
+            Weather(day: "Monday", icon: "sun.min.fill"),
+            Weather(day: "Tuesday", icon: "sun.max.fill"),
+            Weather(day: "Wednesday", icon: "cloud.sun.fill"),
+            Weather(day: "Thursday", icon: "sun.min.fill"),
+            Weather(day: "Friday", icon: "cloud.drizzle.fill"),
+            Weather(day: "Saturday", icon: "cloud.sleet.fill")
+        ]
+    }
+    
+    func updateSunday() {
+        sevenDays[0].day = "Sunday (updated)"
+        sevenDays[0].icon = "cloud.sun.fill"
+    }
+    
+    func updateFriday() {
+        sevenDays[0].day = "Friday (updated)"
+        sevenDays[0].icon = "sun"
+    }
+}
