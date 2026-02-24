651fb6792b76be37a0e6d5c95086bd43dd648c05
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-02-24 10:01:35 -0500

    add WeatherView to Views folder

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Views/WeatherView.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherView.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/24/26.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    
+    @State var oo = ForecastOO()
+    
+    var body: some View {
+        VStack {
+            List(oo.sevenDays) { day in
+                HStack(spacing: 24) {
+                    Image(systemName: day.icon)
+                        .font(.largeTitle)
+                        .foregroundStyle(.blue)
+                    Text(day.day)
+                        .font(.title)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    WeatherView()
+}
