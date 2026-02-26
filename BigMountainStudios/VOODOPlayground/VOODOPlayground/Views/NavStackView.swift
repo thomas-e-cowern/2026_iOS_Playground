1236c116122d78539a9a741f5e05b86129b26e7e
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-26 16:01:23 -0500

    add NavStackView to Views folder

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Views/NavStackView.swift
@@ -0,0 +1,42 @@
+//
+//  NavStackView.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/26/26.
+//
+
+import SwiftUI
+
+struct NavStackView: View {
+    var body: some View {
+        NavigationStack {
+            NavigationLink("View Developer") {
+                DeveloperView()
+            }
+            .navigationTitle("Developer")
+        }
+        .environment(DeveloperOO())
+        .font(.title)
+    }
+}
+
+#Preview {
+    NavStackView()
+}
+
+
+@Observable
+class DeveloperOO {
+    var name: String = "Awesome Developer"
+}
+
+
+struct DeveloperView: View {
+    
+    @Environment(DeveloperOO.self) private var dev
+    
+    var body: some View {
+        Text("Hello, \(dev.name)!")
+            .navigationTitle("Developer View")
+    }
+}
