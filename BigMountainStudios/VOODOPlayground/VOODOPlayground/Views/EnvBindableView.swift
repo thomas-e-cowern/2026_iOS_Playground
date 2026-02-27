59fe42a54deda306d34d5ca348b0544ab6d47cb2
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-27 10:24:10 -0500

    update

--- BigMountainStudios/VOODOPlayground/VOODOPlayground/Views/EnvBindableView.swift
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Views/EnvBindableView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct EnvBindableView: View {
+    
+    @Environment(AddressOO.self) private var addressOO
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            @Bindable var addressBindable = addressOO
+            
+            Section("One Way Binding") {
+                Text("State: \(addressOO.address.state)")
+                    .bold()
+            }
+            
+            Section("Two Way Binding") {
+                TextField("Enter State", text: $addressBindable.address.state)
+            }
+        }
+        .headerProminence(.increased)
     }
 }
 
 #Preview {
     EnvBindableView()
+        .environment(AddressOO())
 }
