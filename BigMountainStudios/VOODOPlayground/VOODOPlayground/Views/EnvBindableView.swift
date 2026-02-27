4a063f012d5707b23f2778e27293879be1ab37b4
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-27 10:13:52 -0500

    add EnvBidnableView to Views folder

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Views/EnvBindableView.swift
@@ -0,0 +1,18 @@
+//
+//  EnvBindableView.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/27/26.
+//
+
+import SwiftUI
+
+struct EnvBindableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnvBindableView()
+}
