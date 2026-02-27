a6026df92254bc595d3935c271ce2b5168c15be7
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-27 10:22:46 -0500

    add Address to Models folder

---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Models/Address.swift
@@ -0,0 +1,12 @@
+//
+//  Address.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/27/26.
+//
+
+import Foundation
+
+struct Address {
+    var state: String
+}
---
+++ BigMountainStudios/VOODOPlayground/VOODOPlayground/Observed/AddressOO.swift
@@ -0,0 +1,8 @@
+//
+//  AdressOO.swift
+//  VOODOPlayground
+//
+//  Created by Thomas Cowern on 2/27/26.
+//
+
+import Foundation
