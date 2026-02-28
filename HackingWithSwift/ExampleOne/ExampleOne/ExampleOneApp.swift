b7211ad10aa0e1f5498a424d7c55cab012331684
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-28 10:37:27 -0500

    add ExampleOne to HackingWithSwift folder

---
+++ HackingWithSwift/ExampleOne/ExampleOne/ExampleOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleOneApp.swift
+//  ExampleOne
+//
+//  Created by Thomas Cowern on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
