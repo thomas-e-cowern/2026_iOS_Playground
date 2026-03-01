e0549635b1f926646ab6f47131b22a5ff2ce808e
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-03-01 08:45:39 -0500

    add Project and Task to Models folder

--- Mine/ToDoAI/ToDoAI/Models/Project.swift
+++ Mine/ToDoAI/ToDoAI/Models/Project.swift
@@ -5,4 +5,14 @@
 //  Created by Thomas Cowern on 2/28/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct Project: Identifiable {
+    let id: UUID              // Unique identifier for the project
+    var title: String         // Title of the project
+    var description: String    // Details about the project
+    var dueDate: Date?        // Optional due date for the project
+    var tasks: [Task]         // List of tasks associated with the project
+    var isCompleted: Bool      // Status of the project (completed or not)
+    var archivedDate: Date?    // Optional date when the project was archived
+}
--- Mine/ToDoAI/ToDoAI/Models/Task.swift
+++ Mine/ToDoAI/ToDoAI/Models/Task.swift
@@ -5,4 +5,20 @@
 //  Created by Thomas Cowern on 2/28/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct Task {
+    let id: UUID              // Unique identifier for the task
+    var title: String         // Title of the task
+    var description: String?   // Optional details about the task
+    var dueDate: Date?        // Optional due date for the task
+    var isCompleted: Bool      // Status of the task (completed or not)
+    var priority: TaskPriority // Priority level of the task
+    var archivedDate: Date?    // Optional date when the task was archived
+}
+
+enum TaskPriority: String {
+    case low
+    case medium
+    case high
+}
