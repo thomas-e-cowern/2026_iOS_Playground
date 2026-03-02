716d6185bdad018433eaa6d1a9502f80ee380a68
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-03-02 12:15:41 -0500

    add ProjectModel to Models folder

---
+++ Mine/ProjectManagerNoAI/ProjectManagerNoAI/Models/ProjectModel.swift
@@ -0,0 +1,40 @@
+//
+//  ProjectModel.swift
+//  ProjectManagerNoAI
+//
+//  Created by Thomas Cowern on 3/2/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class ProjectModel {
+    var name: String
+    var projectDescription: String
+    @Relationship var tasks: [ProjectTask]?
+    
+    init(name: String, projectDescription: String) {
+        self.name = name
+        self.projectDescription = projectDescription
+        self.tasks = []
+    }
+}
+
+@MainActor
+extension ProjectModel {
+    static var preview: ModelContainer {
+        let container = try! ModelContainer(for: ProjectModel.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        
+        container.mainContext.insert(ProjectModel(name: "New Website Launch", projectDescription: "Create and launch a new marketing website."))
+        container.mainContext.insert(ProjectModel(name: "Mobile App Development", projectDescription: "Develop a mobile application for both iOS and Android."))
+        container.mainContext.insert(ProjectModel(name: "E-commerce Platform", projectDescription: "Build an online platform for selling products."))
+        container.mainContext.insert(ProjectModel(name: "Social Media Campaign", projectDescription: "Launch a new social media marketing campaign."))
+        container.mainContext.insert(ProjectModel(name: "Data Analytics Tool", projectDescription: "Develop a tool for analyzing data metrics."))
+        container.mainContext.insert(ProjectModel(name: "SEO Optimization", projectDescription: "Optimize the company's website for search engines."))
+        container.mainContext.insert(ProjectModel(name: "User Experience Research", projectDescription: "Conduct user research to improve product experience."))
+        container.mainContext.insert(ProjectModel(name: "Email Marketing Campaign", projectDescription: "Design and implement an email marketing strategy."))
+      
+        return container
+    }
+}
---
+++ Mine/ProjectManagerNoAI/ProjectManagerNoAI/Models/TaskModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskModel.swift
+//  ProjectManagerNoAI
+//
+//  Created by Thomas Cowern on 3/2/26.
+//
+
+import Foundation
