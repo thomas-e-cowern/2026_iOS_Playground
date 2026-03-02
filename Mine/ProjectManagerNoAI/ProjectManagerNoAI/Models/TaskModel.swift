0a7fdbc8bf84cba31ca87459ec6d0e6423d06bbc
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-03-02 12:16:01 -0500

    add TaskModel to Models folder

--- Mine/ProjectManagerNoAI/ProjectManagerNoAI/Models/TaskModel.swift
+++ Mine/ProjectManagerNoAI/ProjectManagerNoAI/Models/TaskModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class ProjectTask {
+    @Attribute var title: String
+    @Attribute var isCompleted: Bool
+    @Relationship var project: ProjectModel?
+
+    init(title: String, isCompleted: Bool = false) {
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+}
