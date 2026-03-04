9d5a7cec9b7fc1d6fe481f921fab10d3d87e08d4
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-03-04 07:43:04 -0500

    add ProjectRowView to Views folder

---
+++ Mine/ProjectManagerNoAI/ProjectManagerNoAI/Views/ProjectRowView.swift
@@ -0,0 +1,26 @@
+//
+//  ProjectRowView.swift
+//  ProjectManagerNoAI
+//
+//  Created by Thomas Cowern on 3/3/26.
+//
+
+import SwiftUI
+
+struct ProjectRowView: View {
+    
+    var project: ProjectModel
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(project.name)
+                .font(.title)
+            Text(project.projectDescription)
+                .font(.caption)
+        }
+    }
+}
+
+#Preview {
+    ProjectRowView(project: ProjectModel(name: "Project Name", projectDescription: "This is the description"))
+}
