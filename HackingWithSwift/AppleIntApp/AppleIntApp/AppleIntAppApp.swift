e7bbee0659bcc92a8ce5a664ad9a71f286d47dca
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-03-05 10:10:23 -0500

    AppleIntelApp initial commit in HackingWithSwift folder

---
+++ HackingWithSwift/AppleIntApp/AppleIntApp/AppleIntAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleIntAppApp.swift
+//  AppleIntApp
+//
+//  Created by Thomas Cowern on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct AppleIntAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackingWithSwift/AppleIntApp/AppleIntApp/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  AppleIntApp
+//
+//  Created by Thomas Cowern on 3/5/26.
+//
+
+import SwiftUI
+import FoundationModels
+
+struct ContentView: View {
+    
+    @State private var output = ""
+    
+    var body: some View {
+        VStack {
+            Text(output)
+            Button {
+                generateJoke()
+            } label: {
+                Text("Generate a joke")
+            }
+
+        }
+        .padding()
+    }
+    
+    func generateJoke() {
+        print("generating a new joke")
+        output = "generating a new joke"
+        Task {
+            do {
+                let session = LanguageModelSession()
+                let response = try await session.respond(to: "Please tell me a joke")
+                
+                output = response.content
+            } catch {
+                print("Something went wrong in generateJoke()")
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
