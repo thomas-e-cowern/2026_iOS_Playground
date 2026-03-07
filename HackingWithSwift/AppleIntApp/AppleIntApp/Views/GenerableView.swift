327221ac4fb66510b186db7e4473a260ce13a870
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-03-07 13:05:23 -0500

    update GenerableView to add functions

--- HackingWithSwift/AppleIntApp/AppleIntApp/Views/GenerableView.swift
+++ HackingWithSwift/AppleIntApp/AppleIntApp/Views/GenerableView.swift
@@ -6,10 +6,66 @@
 //
 
 import SwiftUI
+import FoundationModels
+
+@Generable
+struct Student: Identifiable {
+    var id: String
+    var firstName: String
+    var lastName: String
+    var age: Int
+    var email: String
+    var phone: String
+    var country: String
+    var address: String
+    var city: String
+}
+
+@Generable
+struct Directory {
+    var students: [Student]
+}
 
 struct GenerableView: View {
+    
+    @State private var session = LanguageModelSession()
+    
+    @State private var directory = Directory(students: [])
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView {
+                if directory.students.count <  1 {
+                    
+                } else {
+                    ForEach(directory.students) { student in
+                        Text("\(student.firstName) \(student.lastName)")
+                    }
+                }
+            }
+            
+            
+            Button {
+                generateADirectory()
+            } label: {
+                Text("Generate Directory")
+            }
+        }
+
+    }
+    
+    func generateADirectory() {
+        Task {
+            do {
+                let response = try await session.respond(to: "Generate a school  directory of 5 students.", generating: Directory.self)
+                
+                print(response)
+                
+                directory = response.content
+            } catch {
+                print("There was an error in generating the directory: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
