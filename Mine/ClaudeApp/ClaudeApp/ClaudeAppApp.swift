6a94202e335357e4e7a1c57728acb5afbee7b3d4
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-03-10 11:57:28 -0400

    ClaudeApp initial commit in Mine folder

---
+++ Mine/ClaudeApp/ClaudeApp/ClaudeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeAppApp.swift
+//  ClaudeApp
+//
+//  Created by Thomas Cowern on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mine/ClaudeApp/ClaudeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClaudeApp
+//
+//  Created by Thomas Cowern on 3/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
