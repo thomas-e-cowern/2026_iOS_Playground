7e537fb7cb614cf77e6fa6523bee798c0534717f
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-03-26 10:15:38 -0400

    add SpaceXView to project

--- Mine/JSONDecoderAI/JSONDecoderAI/SpaceXView.swift
+++ Mine/JSONDecoderAI/JSONDecoderAI/SpaceXView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct SpaceXView: View {
+    
+    @State private var json: JSONValue = .null
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                let thrusters = json.arrayValue ?? []
+                
+                ForEach(Array(thrusters.enumerated()), id: \.offset) { _, product in
+                    LabeledContent("NAME", value: "\(product["name"].stringValue ?? "No name")")
+                }
+            }
+            .task {
+                if let url = Bundle.main.url(forResource: "spacex", withExtension: "json"),
+                   let data = try? Data(contentsOf: url) {
+                    json = JSONValue.decode(from: data)
+                    print(json)
+                }
+            }
+        }
     }
+    
 }
 
 #Preview {
