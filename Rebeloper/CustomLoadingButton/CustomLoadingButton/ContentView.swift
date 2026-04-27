218d375ee3395fb22fb3ff658f1c3b9ff9759c97
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-04-27 10:09:28 -0400

    add CustomLoadingButton project to Rebeloper folder

---
+++ Rebeloper/CustomLoadingButton/CustomLoadingButton/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomLoadingButton
+//
+//  Created by Thomas Cowern on 4/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
