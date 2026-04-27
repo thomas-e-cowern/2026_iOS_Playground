f0777e203ed890b5f318182bee454005fa89babf
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-04-27 10:14:00 -0400

    add LoadingButtonState enum to project

---
+++ Rebeloper/CustomLoadingButton/CustomLoadingButton/LoadingButtonState.swift
@@ -0,0 +1,12 @@
+//
+//  LoadingButtonState.swift
+//  CustomLoadingButton
+//
+//  Created by Thomas Cowern on 4/27/26.
+//
+
+import Foundation
+
+enum LoadingButtonState {
+    case idle, loading, success, error(String)
+}
