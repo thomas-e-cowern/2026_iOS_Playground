e9cce352fb8a0c6a8c021c9fae35ce6ad6cc418d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-04-30 10:19:43 -0400

    add SignInOutView to project

--- Rebeloper/SlideInMenu/SlideInMenu/SignInOutView.swift
+++ Rebeloper/SlideInMenu/SlideInMenu/SignInOutView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct SignInOutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .font(.title)
+            Image(systemName: "door.french.open")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(.blue)
+        }
     }
 }
 
