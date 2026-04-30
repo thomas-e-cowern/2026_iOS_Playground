4b79b3e6ef4feec2bebe7bbd66d10651809f2ac5
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-04-30 10:19:27 -0400

    add SettingsView to project

--- Rebeloper/SlideInMenu/SlideInMenu/SettingsView.swift
+++ Rebeloper/SlideInMenu/SlideInMenu/SettingsView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Settings View")
+                .font(.title)
+            Image(systemName: "gear")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(.blue)
+        }
     }
 }
 
