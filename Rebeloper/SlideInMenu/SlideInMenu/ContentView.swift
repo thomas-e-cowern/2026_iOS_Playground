0050f6eb6b077eb1981e80500d3842495bf5ee48
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-04-30 11:44:19 -0400

    update ContentView to use side menu

--- Rebeloper/SlideInMenu/SlideInMenu/ContentView.swift
+++ Rebeloper/SlideInMenu/SlideInMenu/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showMenu: Bool = false
+    @State private var selectedIndex: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            NavigationStack {
+                Group {
+                    switch selectedIndex {
+                    case 0:
+                        HomeView()
+                    case 1:
+                        SettingsView()
+                    case 2:
+                        ProfileView()
+                    default:
+                        EmptyView()
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            withAnimation(.spring(Spring(response: 0.4, dampingRatio: 0.8))) {
+                                showMenu.toggle()
+                            }
+                        } label: {
+                            Image(systemName: "line.3.horizontal")
+                        }
+
+                    }
+                }
+                .disabled(showMenu)
+            }
+            
+            if showMenu {
+                Color.black.opacity(0.2)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation(.spring) {
+                            showMenu = false
+                        }
+                    }
+            }
+            
+            HStack {
+                VStack {
+                    SideMenuView { index in
+                        
+                        withAnimation(.spring()) {
+                            showMenu = false
+                        }
+                        
+                        if index == 3 {
+                            print("Log Out")
+                        } else {
+                            selectedIndex = index
+                        }
+                    }
+                    .glassEffect()
+                    
+                    Spacer()
+                }
+                .offset(x: showMenu ? 0 : -110)
+                .shadow(radius: 5)
+                .transition(.move(edge: .leading))
+                .padding(.leading, 10)
+                .padding(.top, 50)
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
