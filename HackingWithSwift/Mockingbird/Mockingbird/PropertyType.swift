6f6a6b5f7ae59ec295c949d9c0defb7fd01027dd
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-05-04 10:32:10 -0400

    update PropertyType with DynamicSchema and extractValue function

--- HackingWithSwift/Mockingbird/Mockingbird/PropertyType.swift
+++ HackingWithSwift/Mockingbird/Mockingbird/PropertyType.swift
@@ -13,6 +13,24 @@ enum PropertyType: String, CaseIterable, Codable, Identifiable {
     case integer = "Integer"
     case boolean = "Boolean"
     case double = "Double"
-
+    
     var id: Self { self }
+    
+    var dynamicSchema: DynamicGenerationSchema {
+        switch self {
+        case .string: .init(type: String.self)
+        case .integer: .init(type: Int.self)
+        case .boolean: .init(type: Bool.self)
+        case .double: .init(type: Double.self)
+        }
+    }
+    
+    func extractValue(from item: GeneratedContent, key: String) -> Any? {
+        switch self {
+        case .string: try? item.value(String.self, forProperty: key)
+        case .integer: try? item.value(Int.self, forProperty: key)
+        case .boolean: try? item.value(Bool.self, forProperty: key)
+        case .double: try? item.value(Double.self, forProperty: key)
+        }
+    }
 }
