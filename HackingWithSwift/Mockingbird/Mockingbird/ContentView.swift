fd8ee1027123954ac8e5e5caf9c3cd510eacfc5c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-05-05 09:25:51 -0400

    update ContentView, add properties and prompts with formatting

--- HackingWithSwift/Mockingbird/Mockingbird/ContentView.swift
+++ HackingWithSwift/Mockingbird/Mockingbird/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var properties = [PropertyDefinition]()
+    @State private var prompt: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                Form {
+                    Section("What kind of data are you making?") {
+                        TextField("Enter your prompt...", text: $prompt,
+                                  prompt: Text("e.g. University Students or Fictional Superheroes"), axis: .vertical)
+                        .lineLimit(3...6)
+                        .labelsHidden()
+                    }
+                    
+                    ForEach(properties.enumerated(), id: \.element.id) { index, property in
+                        Section("Property \(index + 1)") {
+                            PropertyRow(property: property)
+                            
+                            Button("Remove Property", systemImage: "trash", role: .destructive) {
+                                properties.removeAll { $0.id == property.id }
+                            }
+                        }
+                    }
+                    
+                    Section {
+                        Button("Add Property", systemImage: "plus") {
+                            properties.append(PropertyDefinition())
+                        }
+                    }
+                }
+                .navigationTitle("Mockingbird")
+                .formStyle(.grouped)
+            }
         }
-        .padding()
     }
 }
 
