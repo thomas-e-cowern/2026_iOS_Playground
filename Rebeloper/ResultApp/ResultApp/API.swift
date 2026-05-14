43e074b05b5ed0ee23e9d6ba7500ed0b226878f2
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-05-14 11:33:11 -0400

    add API to ResultApp

--- Rebeloper/ResultApp/ResultApp/API.swift
+++ Rebeloper/ResultApp/ResultApp/API.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct API {
+    
+    let person1 = DateConverter().randomBirthdateBetween(ageMin: 25, ageMax: 35)
+    let person2 = DateConverter().randomBirthdateBetween(ageMin: 25, ageMax: 35)
+    let person3 = DateConverter().randomBirthdateBetween(ageMin: 25, ageMax: 35)
+    
+    func fetchProfiles(completion: @escaping([Profile]) -> ())  {
+        let profile1 = Profile(name: "John", birthday: person1)
+        let profile2 = Profile(name: "Jane", birthday: person3)
+        let profile3 = Profile(name: "Bob", birthday: person3)
+        
+        let profiles = [profile1, profile2, profile3]
+        completion(profiles)
+    }
+}
