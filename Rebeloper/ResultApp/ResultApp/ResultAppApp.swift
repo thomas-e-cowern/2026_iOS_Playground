9264a61b8cd5c595ffff157b6db070ba4450d9b9
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-05-14 09:31:44 -0400

    add ResultApp to Rebeloper folder

---
+++ Rebeloper/ResultApp/ResultApp/ResultAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResultAppApp.swift
+//  ResultApp
+//
+//  Created by Thomas Cowern on 5/14/26.
+//
+
+import SwiftUI
+
+@main
+struct ResultAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
