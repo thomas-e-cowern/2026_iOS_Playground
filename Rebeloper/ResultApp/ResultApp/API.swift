270798cdc49b20245c9f777bec1d7889f9bdd78a
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-05-14 12:06:18 -0400

    update API to return error

--- Rebeloper/ResultApp/ResultApp/API.swift
+++ Rebeloper/ResultApp/ResultApp/API.swift
@@ -9,16 +9,22 @@ import Foundation
 
 struct API {
     
+    let error: APIError?
+    
     let person1 = DateConverter().randomBirthdateBetween(ageMin: 25, ageMax: 35)
     let person2 = DateConverter().randomBirthdateBetween(ageMin: 25, ageMax: 35)
     let person3 = DateConverter().randomBirthdateBetween(ageMin: 25, ageMax: 35)
     
-    func fetchProfiles(completion: @escaping([Profile]?) -> ())  {
+    func fetchProfiles(completion: @escaping(Result<[Profile], Error>) -> ())  {
         let profile1 = Profile(name: "John", birthday: person1)
         let profile2 = Profile(name: "Jane", birthday: person3)
         let profile3 = Profile(name: "Bob", birthday: person3)
         
         let profiles = [profile1, profile2, profile3]
-        completion(profiles)
+        completion(.success(profiles))
     }
+}
+
+enum APIError: Error {
+    case noData
 }
