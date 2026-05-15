64a3c0063a929945dfc3e20b67661f39b16a9187
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-05-15 07:16:54 -0400

    add SecureLogin to Rebeloper folder

---
+++ Rebeloper/SecureLogin/SecureLogin/SecureLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureLoginApp.swift
+//  SecureLogin
+//
+//  Created by Thomas Cowern on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct SecureLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
