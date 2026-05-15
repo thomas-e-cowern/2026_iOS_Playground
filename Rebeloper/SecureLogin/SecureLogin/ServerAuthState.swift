42e53412f9799b825b425acd5a52a3098650b3ef
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-05-15 07:19:30 -0400

    add ServerAuthState to project

---
+++ Rebeloper/SecureLogin/SecureLogin/ServerAuthState.swift
@@ -0,0 +1,13 @@
+//
+//  ServerAuthState.swift
+//  SecureLogin
+//
+//  Created by Thomas Cowern on 5/15/26.
+//
+
+import Foundation
+
+enum ServerAuthState {
+    case authenticated
+    case notAuthenticated
+}
