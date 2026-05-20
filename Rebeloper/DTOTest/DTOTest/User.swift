519c71716ac64a4b24808675f286b003103aef20
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-05-20 10:49:38 -0400

    add User modelt to DTOTest project

---
+++ Rebeloper/DTOTest/DTOTest/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  DTOTest
+//
+//  Created by Thomas Cowern on 5/20/26.
+//
+
+import Foundation
+
+struct User {
+    let id: Int
+    let name: String
+    let email: String
+    let createdAt: Date
+}
