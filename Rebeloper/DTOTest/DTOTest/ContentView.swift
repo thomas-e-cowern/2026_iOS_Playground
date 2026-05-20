77d22b925c6e93e86fbaa8ae143ef0c7c91a940e
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-05-20 10:47:42 -0400

    initial commit of DTOTest in Reboloper folder

---
+++ Rebeloper/DTOTest/DTOTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DTOTest
+//
+//  Created by Thomas Cowern on 5/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rebeloper/DTOTest/DTOTest/DTOTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTOTestApp.swift
+//  DTOTest
+//
+//  Created by Thomas Cowern on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct DTOTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
