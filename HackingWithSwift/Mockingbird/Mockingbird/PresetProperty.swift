33b4555411a87e6163ac3e22ea2007804e8a2803
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-05-21 14:16:21 -0400

    add PresetProperty to Mockingbird app

---
+++ HackingWithSwift/Mockingbird/Mockingbird/PresetProperty.swift
@@ -0,0 +1,14 @@
+//
+//  PresetProperty.swift
+//  Mockingbird
+//
+//  Created by Thomas Cowern on 5/21/26.
+//
+
+import Foundation
+
+struct PresetProperty: Codable, Hashable {
+    var name: String
+    var type: String
+    var description: String
+}
