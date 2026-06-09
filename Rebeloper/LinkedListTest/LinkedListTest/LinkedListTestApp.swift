e416d6ed40523d59f158368e43c9d380473cc69c
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-06-09 15:54:41 -0400

    LinkedListTest initial commit

---
+++ Rebeloper/LinkedListTest/LinkedListTest/LinkedListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedListTestApp.swift
+//  LinkedListTest
+//
+//  Created by Thomas Cowern on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct LinkedListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
