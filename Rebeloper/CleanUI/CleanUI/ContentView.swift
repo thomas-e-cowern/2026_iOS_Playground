850ea373757e57a9de2bcf4cfb5eac8dd260e91c
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-06-25 09:00:14 -0400

    update ContentView with switch statement and load data function

--- Rebeloper/CleanUI/CleanUI/ContentView.swift
+++ Rebeloper/CleanUI/CleanUI/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var state: ViewState<[String]> = .loading
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            switch state {
+            case .loading:
+                ProgressView("Loading...")
+            case .empty:
+                Text("No data")
+            case .error(let string):
+                Text(string)
+                Button {
+                    Task {
+                        await loadData()
+                    }
+                } label: {
+                    Text("Retry")
+                }
+            case .success(let data):
+                List(data, id: \.self) {
+                    Text($0)
+                }
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        state = .loading
+        
+        do {
+            try await Task.sleep(for: .seconds(2))
+            
+            let data = ["Hello", "World", "This", "Is", "A", "Test"]
+            
+            if data.isEmpty {
+                state = .empty
+            } else {
+                state = .success(data)
+            }
+        } catch  {
+            print("There was an eror: \(error.localizedDescription)")
+        }
     }
 }
 
