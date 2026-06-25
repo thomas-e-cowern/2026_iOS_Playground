ca02643f06d8156bc96d56b772d33395637f5d61
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-06-25 08:35:55 -0400

    add CleanUI to Rebeloper folder

---
+++ Rebeloper/CleanUI/CleanUI/CleanUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanUIApp.swift
+//  CleanUI
+//
+//  Created by Thomas Cowern on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct CleanUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
