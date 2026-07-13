6d95fc33869d1a9094b89ffc49c6fcbd7317c047
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-07-13 13:35:51 -0400

    update ContentView with imports and OnboardingView

--- Other/OnBoardingApp/OnBoardingApp/ContentView.swift
+++ Other/OnBoardingApp/OnBoardingApp/ContentView.swift
@@ -6,6 +6,38 @@
 //
 
 import SwiftUI
+import CoreLocation
+import UserNotifications
+import AppTrackingTransparency
+
+struct OnboardingView: View {
+    
+    @State private var currentPage: Int = 0
+    @State private var isOnboardingComplete: Bool = false
+    @StateObject private var locationManager = LocationManager()
+    @State private var notificationPermissionGranted: Bool = false
+    
+    private let gradientColors = [
+        Color.blue, Color.purple
+    ]
+    
+    var body: some View {
+        Group {
+            if isOnboardingComplete {
+                MaincontentView()
+            } else {
+                ZStack {
+                    AnimatedGradient(colors: gradientColors)
+                        .ignoresSafeArea()
+                    onboardingInterface
+                }
+                .onAppear(perform: setupLocationObserver)
+            }
+        }
+    }
+}
+
+
 
 struct ContentView: View {
     var body: some View {
