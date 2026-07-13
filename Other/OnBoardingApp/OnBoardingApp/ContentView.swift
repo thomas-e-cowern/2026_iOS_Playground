aacfaaa4f9c0215490f7592dacba3521175d1ab0
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-07-13 13:23:04 -0400

    OnBoardingApp initial commit

---
+++ Other/OnBoardingApp/OnBoardingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnBoardingApp
+//
+//  Created by Thomas Cowern on 7/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Other/OnBoardingApp/OnBoardingApp/OnBoardingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingAppApp.swift
+//  OnBoardingApp
+//
+//  Created by Thomas Cowern on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
