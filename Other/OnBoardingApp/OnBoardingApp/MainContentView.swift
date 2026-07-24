b6ba7908d28cdbd07155204c8ebb22ebdb30359a
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-07-24 09:49:54 -0400

    add MainContentView to project

---
+++ Other/OnBoardingApp/OnBoardingApp/MainContentView.swift
@@ -0,0 +1,16 @@
+//
+//  MainContentView.swift
+//  OnBoardingApp
+//
+//  Created by Thomas Cowern on 7/24/26.
+//
+
+import SwiftUI
+
+struct MainContentView: View {
+    var body: some View {
+        Text("Welcome to the app")
+            .font(.largeTitle)
+            .padding()
+    }
+}
