5e6eaa4366267fdf983c2904d06661da889ed235
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-07-28 03:27:58 -0400

    EnvironmentPlayground initial commit in AS folder

---
+++ AS/EnvironmentPlayground/EnvironmentPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnvironmentPlayground
+//
+//  Created by Thomas Cowern on 7/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ AS/EnvironmentPlayground/EnvironmentPlayground/EnvironmentPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentPlaygroundApp.swift
+//  EnvironmentPlayground
+//
+//  Created by Thomas Cowern on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
