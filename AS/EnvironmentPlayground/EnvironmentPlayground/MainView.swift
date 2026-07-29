17bcc8a4ca53337209e409dbf9b588e606e66f62
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-07-29 01:33:04 -0400

    add MainView to EnvironmentPlayground

---
+++ AS/EnvironmentPlayground/EnvironmentPlayground/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  EnvironmentPlayground
+//
+//  Created by Thomas Cowern on 7/29/26.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
