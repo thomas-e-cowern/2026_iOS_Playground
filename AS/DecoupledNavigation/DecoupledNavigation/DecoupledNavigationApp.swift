4b5ee4740334e0b760b996eddf23af14ef7cea12
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-07-30 00:33:01 -0400

    DecoupledNavigation initial commit

---
+++ AS/DecoupledNavigation/DecoupledNavigation/DecoupledNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecoupledNavigationApp.swift
+//  DecoupledNavigation
+//
+//  Created by Thomas Cowern on 7/30/26.
+//
+
+import SwiftUI
+
+@main
+struct DecoupledNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
